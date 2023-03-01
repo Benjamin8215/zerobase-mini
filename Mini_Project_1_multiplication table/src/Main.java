c359b31544e859bb73f89e9d5f28abaf08311550
Benjamin8215 <124556623+Benjamin8215@users.noreply.github.com>
2023-03-01 23:08:51 +0900

    Add files via upload

---
+++ Mini_Project_1_multiplication table/src/Main.java
@@ -0,0 +1,17 @@
+/*
+정영석
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("[구구단 출력]");
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.print(j + " * " + i + " = " + String.format("%2d", i * j));
+                System.out.print("    ");
+            }
+            System.out.println();
+        }
+    }
+}
+
